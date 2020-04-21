4bb10e4fc5a2f42facd69e3508b9afcd516c27e3
cptCloud <45497923+cptCloud@users.noreply.github.com>
2020-04-21 12:42:32 -0400

    GraphQL the API call

--- getissues.js
+++ getissues.js
@@ -5,33 +5,38 @@ container.setAttribute('class', 'container');
 
 app.appendChild(container);
 
+var data_query = "{\"query\":\"query {\\n repository(owner:\\\"transitco\\\", name:\\\"roadmap\\\") {\\n issues(last:20, states:OPEN) {\\n edges {\\n node {\\n title\\n url\\n body\\n }\\n }\\n }\\n }\\n}\"}";
+
 var request = new XMLHttpRequest();
-request.open('GET', 'https://api.github.com/repos/transitco/roadmap/issues', true);
+
+request.open("POST", "https://api.github.com/graphql");
+request.setRequestHeader("content-type", "application/json");
+request.responseType = 'json';
 request.onload = function () {
 
-  // Begin accessing JSON data here
-  var data = JSON.parse(this.response);
-  if (request.status >= 200 && request.status < 400) {
-    data.forEach(issue => {
-      const card = document.createElement('div');
-      card.setAttribute('class', 'card');
+// Begin accessing JSON data here
+var data = this.response;
+if (request.status >= 200 && request.status < 400) {
+data.data.repository.issues.edges.forEach(issue => {
+const card = document.createElement('div');
+card.setAttribute('class', 'card');
 
-      const h2 = document.createElement('h2');
-      h2.textContent = issue.title;
+const h2 = document.createElement('h2');
+h2.textContent = issue.node.title;
 
-      const p = document.createElement('p');
-      issue.body = issue.body.substring(0, 300);
-      p.textContent = `${issue.body}...`;
+const p = document.createElement('p');
+issue.node.body = issue.node.body.substring(0, 300);
+p.textContent = `${issue.node.body}...`;
 
-      container.appendChild(card);
-      card.appendChild(h2);
-      card.appendChild(p);
-    });
-  } else {
-    const errorMessage = document.createElement('marquee');
-    errorMessage.textContent = `Gah, it's not working!`;
-    app.appendChild(errorMessage);
-  }
+container.appendChild(card);
+card.appendChild(h2);
+card.appendChild(p);
+});
+} else {
+const errorMessage = document.createElement('marquee');
+errorMessage.textContent = `Gah, it's not working!`;
+app.appendChild(errorMessage);
+}
 }
 
-request.send();
+request.send(data_query);
